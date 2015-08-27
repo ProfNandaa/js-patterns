dba83928d3072cfe89d95354d16588d84de1bd72
@ProfNandaa <profnandaa@gmail.com>
2015-08-27 23:27:13 +0300

    Decorator Pattern

--- design_patterns.js
+++ design_patterns.js
@@ -186,3 +186,105 @@ while (agg.hasNext()) {
 agg.rewind();
 
 console.log(agg.current());
+
+/*-- Decorator --*/
+/*
+In the decorator pattern, additional functionality can 
+be added to an object dynamically, at runtime.
+*/
+
+// Example
+function Sale(price) {
+	this.price = price || 100;
+}
+
+Sale.prototype.getPrice = function () {
+	return this.price;
+}
+
+// The decorator objects will be implemented as properties
+// of a constructor property:
+
+Sale.decorators = {};
+
+Sale.decorators.fedtax = {
+	getPrice: function () {
+		var price = this.uber.getPrice();
+		price += price * 5 / 100;
+		return price;
+	}
+};
+
+Sale.decorators.money = {
+	getPrice: function () {
+		return "$" + this.uber.getPrice().toFixed(2);
+	}
+};
+
+Sale.decorators.cdn = {
+	getPrice: function () {
+		return "CDN$" + this.uber.getPrice().toFixed(2);
+	}
+}
+
+Sale.protototype.decorate = function (decorator) {
+	var F = function () {},
+			overrides = this.constructor.decorators[decorator],
+			i, newobj;
+
+	F.prototype = this;
+	newobj = new F();
+	newobj.uber = F.prototype;
+	for (i in overrides) {
+		if (overrides.hasOwnProperty(i)) {
+			newobj[i] = overrides[i];
+		}
+	}
+
+	return newobj;
+}
+
+// implementation using a list
+
+function SaleX(price) {
+	this.price = (price > 0) ? price : 100;
+	this.decorators_list = [];
+}
+
+SaleX.decorators = {};
+
+SaleX.decorators.fedtax = {
+	getPrice: function (price) {
+		return price + price * 5 / 100;
+	}
+};
+
+SaleX.decorators.quebec = {
+	getPrice: function (price) {
+		return price + price * 7.5 / 100;
+	}
+};
+
+SaleX.decorators.money = {
+	getPrice: function (price) {
+		return "$" + price.toFixed(2);
+	}
+};
+
+SaleX.prototype.decorate = function (decorator) {
+	this.decorators_list.push(decorator);
+};
+
+SaleX.prototype.getPrice = function () {
+	var price = this.price,
+			i,
+			max = this.decorators_list.length,
+			name;
+
+	for (i = 0; i < max; i += 1) {
+		name = this.decorators_list[i];
+		price = SaleX.decorators[name].getPrice(price);
+	}
+
+	return price;
+}
