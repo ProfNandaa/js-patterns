577b8380952494cbbb5a9c0940cc5b27a6ab4517
@ProfNandaa <profnandaa@gmail.com>
2015-08-27 22:41:36 +0300

    Iterator pattern

--- design_patterns.js
+++ design_patterns.js
@@ -145,3 +145,44 @@ o.constructor === Object; // true
 n.constructor === Number; // true
 s.constructor === String; // true
 b.constructor === Boolean; // true
+
+
+/*-- Iterator --*/
+
+// Example
+var agg = (function () {
+
+	var index = 0,
+			data = [1, 2, 3, 4, 5],
+			length = data.length;
+
+	return {
+		next: function () {
+			var element;
+			if (!this.hasNext()) {
+				return null;
+			}
+			element = data[index];
+			index = index + 2;
+			return element;
+		},
+		hasNext: function () {
+			return index < length;
+		},
+		rewind: function () {
+			index = 0;
+		},
+		current: function () {
+			return data[index];
+		}
+	};
+}());
+
+while (agg.hasNext()) {
+	console.log(agg.next());
+}
+
+// go back
+agg.rewind();
+
+console.log(agg.current());
